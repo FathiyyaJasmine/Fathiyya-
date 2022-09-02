94847889d73d073306b5103696ce32b441d09ac4
FathiyyaJasmine <112569446+FathiyyaJasmine@users.noreply.github.com>
2022-09-02 09:06:27 +0700

    Add files via upload

---
+++ FathiyyaJasmine.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main () {
+	string a;
+	cout<<"Nama : ";
+	cin>>a;
+	
+	string b;
+	cout<<"NPM : ";
+	cin>>b;
+	
+	string c;
+	cout<<"Alamat : ";
+	cin>>c;
+	
+	string d;
+	cout<<"Hobi :";
+	cin>>d;
+	
+	cout<<"Nama saya adalah : "<<a<<endl;
+	cout<<"NPM saya adalah : "<<b<<endl;
+	cout<<"Alamat saya adalah : "<<c<<endl;
+	cout<<"Hobi saya adalah : "<<d<<endl;
+}
+
